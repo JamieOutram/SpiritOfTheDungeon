078600017ffdb4ca8600e80e1bb41ad5d14f995e
jpo1g16 <jpo1g16@soton.ac.uk>
2020-06-29 21:15:14 +0100

    create scene and add collier test script

---
+++ Assets/Scripts/collider_test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collider_test : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float forceMagnitude = 200f;
+    private Vector2 rightForce = new Vector2();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rightForce = new Vector2(forceMagnitude * Time.deltaTime, 0f);
+        rb.AddForce(rightForce);
+    }
+}
