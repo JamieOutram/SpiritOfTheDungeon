3145d6f88b6fe7321299787d897ed0d3feac9a7d
ShiftyBump <alex.miles@trinity.ox.ac.uk>
2020-07-01 13:47:21 +0100

    Started trying to make a script that generates rooms

--- Assets/Scripts/world_gen/room_generator.cs
+++ Assets/Scripts/world_gen/room_generator.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
 
 public class room_generator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public RectInt roomBase = new RectInt()
 
     // Update is called once per frame
     void Update()
