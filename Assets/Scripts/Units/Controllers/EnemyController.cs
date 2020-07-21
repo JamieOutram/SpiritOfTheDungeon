127f028cbd4f0c5ccf25a40938ba45a74b073eec
jpo1g16 <jpo1g16@soton.ac.uk>
2020-07-21 17:59:32 +0100

    add Astar pathfinding package

---
+++ Assets/Scripts/Units/Controllers/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : Unit_Control_Base
+{
+    public float lookRadius;
+    
+    [SerializeField] private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
