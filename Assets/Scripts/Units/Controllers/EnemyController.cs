83388b13f996507d802dcfdd6cd2f1307d5853e1
jpo1g16 <jpo1g16@soton.ac.uk>
2020-07-23 16:20:43 +0100

    add EnemyControllerScript

--- Assets/Scripts/Units/Controllers/EnemyController.cs
+++ Assets/Scripts/Units/Controllers/EnemyController.cs
@@ -1,17 +1,31 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : Unit_Control_Base
 {
     public float lookRadius;
-    
-    [SerializeField] private Transform target;
+    public float speed = 500f;
+    public Transform target;
+
+    private Path path;
+    private Seeker seeker;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        seeker = GetComponent<Seeker>();
+        seeker.StartPath(gameObject.transform.position, target.position, OnPathComplete);
+    }
+
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+        }
     }
 
     // Update is called once per frame
