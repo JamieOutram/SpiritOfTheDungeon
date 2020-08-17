8dbebf084b836c52e5b90b13366c44ffc9af8344
JamieOutram <jamie.outram@btinternet.com>
2020-08-17 19:14:07 +0100

    Fix InfoBox positioning relative to mouse

--- Assets/Scripts/Utils/UIUtils.cs
+++ Assets/Scripts/Utils/UIUtils.cs
@@ -6,11 +6,13 @@ public static class UIUtils
 {
     public static void ClampRectToRect(RectTransform innerTransform, RectTransform containerTransfrom)
     {
-        Vector2 infoSizeDelta = new Vector2(innerTransform.rect.width, innerTransform.rect.height);
-        Vector2 canvasSizeDelta = new Vector2(containerTransfrom.rect.width, containerTransfrom.rect.height);
+        Vector2 innerDelta = new Vector2(innerTransform.rect.width, innerTransform.rect.height);
+        Vector2 containerDelta = new Vector2(containerTransfrom.rect.width, containerTransfrom.rect.height);
         Vector2 apos = innerTransform.anchoredPosition;
-        Vector2 max = canvasSizeDelta * (Vector2.one - containerTransfrom.pivot) - infoSizeDelta * (Vector2.one - innerTransform.pivot);
-        Vector2 min = infoSizeDelta * innerTransform.pivot - canvasSizeDelta * containerTransfrom.pivot;
+        Vector2 max = containerDelta * (Vector2.one - containerTransfrom.pivot) - innerDelta * (Vector2.one - innerTransform.pivot);
+        Vector2 min = innerDelta * innerTransform.pivot - containerDelta * containerTransfrom.pivot;
+        Debug.Log(string.Format("Min: {0}", min));
+        Debug.Log(string.Format("Max: {0}", max));
         apos.x = Mathf.Clamp(apos.x, min.x, max.x);
         apos.y = Mathf.Clamp(apos.y, min.y, max.y);
         innerTransform.anchoredPosition = apos;
