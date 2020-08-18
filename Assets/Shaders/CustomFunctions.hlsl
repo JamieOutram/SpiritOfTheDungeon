2dabfd6717d23f911b1e1ce696b35029bd875eb0
JamieOutram <jamie.outram@btinternet.com>
2020-08-18 19:28:02 +0100

    Add Shader for HealthBar

---
+++ Assets/Shaders/CustomFunctions.hlsl
@@ -0,0 +1,16 @@
+﻿#ifndef CUSTOMFUNCTIONS_INCLUDED
+#define CUSTOMFUNCTIONS_INCLUDED
+
+void XorYConditional_float(float A, float B, float T, out float Out) 
+{
+	if (T == 0) 
+	{
+		Out = A;
+	}
+	else 
+	{
+		Out = B;
+	}
+}
+
+#endif
