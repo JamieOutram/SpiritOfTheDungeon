75f7342b2620216bfbb2c0f9beddda6a12ef47df
JamieOutram <jamie.outram@btinternet.com>
2020-10-24 16:23:07 +0100

    add new DerivedUnitStat formulas

--- Assets/Scripts/Units/Stats/DerivedUnitStat.cs
+++ Assets/Scripts/Units/Stats/DerivedUnitStat.cs
@@ -8,7 +8,7 @@ public class DerivedUnitStat : UnitStat
 {
     private bool _isInitial = true;
 
-    public DerivedUnitStat(float bValue, UnitStatType sType) : base(bValue, sType) {}
+    public DerivedUnitStat(float bValue, UnitStatType sType) : base(bValue, sType) { }
 
     private StatModifier baseModifier;
     public void UpdateValue(float unitStatValue)
@@ -26,6 +26,39 @@ public class DerivedUnitStat : UnitStat
             case UnitStatType.MaxMana: //Maximum Mana formula
                 baseModifier = new StatModifier(unitStatValue * 10, StatModType.Flat);
                 break;
+
+            case UnitStatType.PhysDefAmp: //Physical Defence multiplier formula
+                baseModifier = new StatModifier(1f - (unitStatValue * 0.1f) / (1f + unitStatValue * 0.1f), StatModType.PercentMult);
+                break;
+
+            case UnitStatType.PhysDefFlat: //Physical Defence bonus formula
+                baseModifier = new StatModifier(unitStatValue * 5f, StatModType.Flat);
+                break;
+
+            case UnitStatType.PhysDmgAmp: //Physical Attack multiplier formula
+                baseModifier = new StatModifier(unitStatValue * 0.05f, StatModType.PercentAdd);
+                break;
+
+            case UnitStatType.PhysDmgFlat: //Physical Attack bonus formula
+                baseModifier = new StatModifier(unitStatValue * 10f, StatModType.Flat);
+                break;
+
+            case UnitStatType.MagiDefAmp: //Magical Defence multiplier formula
+                baseModifier = new StatModifier(1f - (unitStatValue * 0.1f) / (1f + unitStatValue * 0.1f), StatModType.PercentMult);
+                break;
+
+            case UnitStatType.MagiDefFlat: //Magical Defence bonus formula
+                baseModifier = new StatModifier(unitStatValue * 5f, StatModType.Flat);
+                break;
+
+            case UnitStatType.MagiDmgAmp: //Magical Attack multiplier formula
+                baseModifier = new StatModifier(unitStatValue * 0.05f, StatModType.PercentAdd);
+                break;
+
+            case UnitStatType.MagiDmgFlat: //Magical Attack bonus formula
+                baseModifier = new StatModifier(unitStatValue * 10f, StatModType.Flat);
+                break;
+
             default:
                 Debug.LogError("Invalid stat used with DerivedUnitStat class instance.");
                 throw new ArgumentException("Invalid stat used with DerivedUnitStat class instance.");
