55b56963c89832d87f2cf68d5f00f3f3ece63479
JamieOutram <jamie.outram@btinternet.com>
2020-11-10 19:30:57 +0000

    Add particle system fade script

---
+++ Assets/Scripts/UI/AnimationScripts/FadeParticleSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeParticleSystem : FadeBase
+{
+    ParticleSystem ps = default;
+    protected override void A_FadeIn()
+    {
+        ps.Play(true);
+    }
+
+    protected override void A_FadeOut()
+    {
+        ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+    }
+
+    protected override void A_OnAwake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    protected override void A_OnFadeInComplete()
+    {
+        return;
+    }
+
+    protected override void A_OnFadeOutComplete()
+    {
+        return;
+    }
+
+    protected override void A_ResetInvisible()
+    {
+        ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+    }
+
+    protected override void A_ResetVisible()
+    {
+        ps.Play(true);
+    }
+}
